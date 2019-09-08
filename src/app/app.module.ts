6056741e4905c59e77e54d7958d6e5b626ddd222
Mohamed Naim Guerroui <mohamednaim.guerroui@neoxia.com>
2019-09-08 12:39:19 +0200

    fix ng-bootstrap: update angular dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { RouterModule } from '@angular/router';
+import { SliderComponent } from './slider/slider.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    SliderComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    // NgbModule.forRoot()
+    NgbModule.forRoot(),
     RouterModule.forRoot([
       { path: 'home', component: HomeComponent },
     //  { path: 'page-test', component: PageTestComponent}, 
