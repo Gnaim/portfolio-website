d44df75cf701f3efe0df378a6e3fa7f1333ff766
Mohamed Naim Guerroui <mohamednaim.guerroui@neoxia.com>
2019-09-08 04:21:34 +0200

    init navbar and home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule.forRoot()
+    // NgbModule.forRoot()
+    RouterModule.forRoot([
+      { path: 'home', component: HomeComponent },
+    //  { path: 'page-test', component: PageTestComponent}, 
+      { path: '**', redirectTo: '/home', pathMatch: 'full' }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
